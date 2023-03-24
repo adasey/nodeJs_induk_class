cdf64c06d7d9b7b45e17d33d0160dd8ee0e65a6c
이민욱 <50089403+adasey@users.noreply.github.com>
2023-03-24 21:07:00 +0900

    [fix] this test

--- learn/ch3/3.4/require.js
+++ learn/ch3/3.4/require.js
@@ -1,9 +1,12 @@
 console.log('require가 가장 위에 오지 않아도 됩니다.');
 
 module.exports = '저를 찾아보세요.';
+exports = 'this';
 
 require('./var');
 
+console.log(this);
+console.log(module.exports);
 console.log('require.cache입니다.');
 console.log(require.cache);
 console.log('require.main입니다.');
